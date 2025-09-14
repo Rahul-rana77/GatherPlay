0c6e116f2b8c2c89e8cd7ff448eccb47e49b33ef
Rahul Rana <rahulranawork11@gmail.com>
2025-09-15 02:26:51 +0530

    login url

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "./index.html"),
-        login: resolve(__dirname, "/login"),
+        login: resolve(__dirname, "./src/pages/login/login.html"),
         home: resolve(__dirname, "./src/pages/home/home.html"),
         createRoom: resolve(__dirname, "./src/pages/create-room/create.html"),
         joinRoom: resolve(__dirname, "./src/pages/join-room/join.html"),
