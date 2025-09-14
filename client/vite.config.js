83d2ca2bf37aaa7c4e71aae80bd58c814a97ae78
Rahul Rana <rahulranawork11@gmail.com>
2025-09-15 01:07:33 +0530

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,21 +2,16 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  root: "client",
+  root: ".", // client folder root
   build: {
-    outDir: "../dist", // build output outside src
     rollupOptions: {
       input: {
-        signup: resolve(__dirname, "./index.html"),
+        main: resolve(__dirname, "./index.html"),
         login: resolve(__dirname, "./src/pages/login/login.html"),
         home: resolve(__dirname, "./src/pages/home/home.html"),
         createRoom: resolve(__dirname, "./src/pages/create-room/create.html"),
         joinRoom: resolve(__dirname, "./src/pages/join-room/join.html"),
       },
     },
-  },
-  server: {
-    port: 5173,
-    open: true, // automatically open browser
   },
 });
