c4d34136818b05788c6e338cc64772a183c0d6a2
Rahul Rana <rahulranawork11@gmail.com>
2025-09-15 18:28:19 +0530

    fixed some bugs

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,14 @@ import { resolve } from "path";
 export default defineConfig({
   root: ".", // client folder root
   build: {
+    outDir: "dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "./index.html"),
-        login: resolve(__dirname, "./src/pages/login/login.html"),
-        home: resolve(__dirname, "./src/pages/home/home.html"),
-        createRoom: resolve(__dirname, "./src/pages/create-room/create.html"),
-        joinRoom: resolve(__dirname, "./src/pages/join-room/join.html"),
+        main: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "src/pages/login/login.html"),
+        home: resolve(__dirname, "src/pages/home/home.html"),
+        createRoom: resolve(__dirname, "src/pages/create-room/create.html"),
+        joinRoom: resolve(__dirname, "src/pages/join-room/join.html"),
       },
     },
   },
