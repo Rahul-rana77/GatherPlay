2923219feb9e73843b1fa21dc7dd8e8717fe482f
Rahul Rana <rahulranawork11@gmail.com>
2025-11-05 17:23:48 +0530

    video calling feature

--- client/src/pages/room.jsx
+++ client/src/pages/room.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io("https://gatherplay.onrender.com");
 
 export default function Room({ roomId }) {
   const localVideo = useRef();
